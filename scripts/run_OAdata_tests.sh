cf46cb3b0360614b7976d1ba381200ec44b81fdc
Tanner C. Myers <tannercmyers@gmail.com>
2021-06-30 16:56:06 -0500

    Commenting out the qsub command from within script

--- scripts/run_OAdata_tests.sh
+++ scripts/run_OAdata_tests.sh
@@ -1,16 +1,16 @@
 #! /usr/bin/env bash
 
-qsub -N OA-tests \
-	-n \
-	-d /home/tcm0036/OA_2021_AU/scripts/ \
-	-q gen28 \
-	-W group_list=jro0014_lab \
-	-W x=FLAGS:ADVRES:jro0014_s28 \
-	-l nodes=1:ppn=1,mem=120gb,walltime=48:00:00 <<<"
+#qsub -N OA-tests \
+#	-n \
+#	-d /home/tcm0036/OA_2021_AU/scripts/ \
+#	-q gen28 \
+#	-W group_list=jro0014_lab \
+#	-W x=FLAGS:ADVRES:jro0014_s28 \
+#	-l nodes=1:ppn=1,mem=120gb,walltime=48:00:00 <<<"
 	
 	# Load conda environment with R
 	source ~/mambaforge/etc/profile.d/conda.sh # Or path to where your conda is
 	conda activate R_env
 
 	Rscript analyze_OAdata_testsonly.R
-	"
+#	"
