d86cc444332a70d8bde1ce56181e60d7c6a641d8
Laurie Stevison <lss0021@CSM-LSS0021-M1MINI.local>
2021-07-07 16:05:58 -0500

    playing around with matched data

--- scripts/analyze_OAdata_matched.R
+++ scripts/analyze_OAdata_matched.R
@@ -13,6 +13,10 @@ library(nlme)
 #install.packages('lme4')
 library(lme4)
 library(dplyr)
+#install.packages('lmerTest')
+library(lmerTest)
+#install.packages('car')
+library(car)
 
 
 # the following code takes matched data
@@ -33,12 +37,12 @@ head(matched)
 summary(matched)
 
 #try to summarize 
-matched2=summaryBy(norm_cit~journal+OAlab+vol_issue, data=matched,FUN=c(mean,sum,length))
+matched2=summaryBy(norm_cit~jour+OAlab+vol_issue, data=matched,FUN=c(mean,sum,length))
 
 #reshape data to have column for each type of access
-m3=dcast(matched2,vol_issue+journal~OAlab,value.var="norm_cit.sum")
-m4=dcast(matched2,vol_issue+journal~OAlab,value.var="norm_cit.length")
-m5=dcast(matched2,vol_issue+journal~OAlab,value.var="norm_cit.mean")
+m3=dcast(matched2,vol_issue+jour~OAlab,value.var="norm_cit.sum")
+m4=dcast(matched2,vol_issue+jour~OAlab,value.var="norm_cit.length")
+m5=dcast(matched2,vol_issue+jour~OAlab,value.var="norm_cit.mean")
 
 #add count columns to m5 dataframe
 m5$CA.count=m4$`Closed Access`
@@ -78,24 +82,34 @@ md$APC <- as.numeric(gsub("[\\$,]", "", md$APC))
 md=md[,c(1:8)]
 
 #join metadata with clean data
-merged <- merge(m5, md, by.x = "journal",by.y="jour")
+merged <- merge(m5, md, by="jour")
 
-
+#summarize by field (this is an absolute mess!)
+green=tapply(merged$green_cit_diff,merged$field,mean,na.rm=T)
+paid=tapply(merged$paid_cit_diff,merged$field,mean,na.rm=T)
+w=as.data.frame(cbind(green,paid))
+w$paid_rank=rank(w$paid,na.last="keep")
+w$green_rank=rank(w$green,na.last="keep")
+plot(w$paid_rank,w$green_rank)
+text(w$paid_rank, w$green_rank, labels = row.names(w[1:12,]), pos = 4)
+abline(0.5,0.8)
 #now we are ready to do some stats!
 # Recode categorical fields (deviation-compares level to grand mean)
-contrasts(matched$field) = contr.sum(12)
-contrasts(matched$jour_loc) =contr.sum(15)
+#contrasts(matched$field) = contr.sum(12)
+#contrasts(matched$jour_loc) =contr.sum(12)
+#contrasts(matched$OAlab) =contr.sum(4)
+matched$OAlab=as.factor(matched$OAlab)
 
 #do NOT run this! need to work on the model a bit!
-fit <- glmer(norm_cit~OAlab+field+JCR_quart+jour_loc+(1|field:jour), #+(1|journal:vol_issue), 
-           data = matched, family=poisson)
-summary(fit)
+#fit <- glmer(norm_cit~OAlab+field+JCR_quart+jour_loc+(1|field:jour), #+(1|journal:vol_issue), 
+ #          data = matched, family=poisson)
+#summary(fit)
 
 #from Amanda's code
-mod2.1 <- glmer(norm_cit~relevel(OAlab, ref = "Closed Access")+auth_count+JCR_quart+AIS+(1|field:jour), 
-                data = datum, family = poisson)
+mod2.1 <- glmer(norm_cit~relevel(OAlab, ref = "Closed Access")+JCR_quart+AIS+(1|jour:vol_issue), 
+                data = matched, family = poisson)
 summary(mod2.1)
-
+Anova(mod2.1)
 
 
 #quick plots
@@ -112,7 +126,7 @@ data_summary <- function (datum) {
 }
 # violin plot: Citations by JIF quartile PAID
 
-vplot <- ggplot(m5,aes(x=as.factor(JCR_quart),y=paid_cit_diff,fill=as.factor(JCR_quart))) +
+vplot <- ggplot(merged,aes(x=as.factor(JCR_quart),y=paid_cit_diff,fill=as.factor(JCR_quart))) +
   geom_violin(trim=FALSE) 
 vplot <- vplot + ggtitle("Paid Citation Advantage by JIF Quartile") +
   xlab("Journal Impact Factor Quartile (Q1>Q4)") + ylab("Citation Advantage Paid") + 
@@ -122,10 +136,29 @@ vplot
 
 # violin plot: Citations by JIF quartile FREE
 
-vplot <- ggplot(m5,aes(x=as.factor(JCR_quart),y=free_cit_diff,fill=as.factor(JCR_quart))) +
+vplot <- ggplot(merged,aes(x=as.factor(JCR_quart),y=free_cit_diff,fill=as.factor(JCR_quart))) +
   geom_violin(trim=FALSE) 
 vplot <- vplot + ggtitle("Free Citation Advantage by JIF Quartile") +
   xlab("Journal Impact Factor Quartile (Q1>Q4)") + ylab("Citation Advantage Free") + 
+  theme(legend.position="none", plot.title=element_text(hjust = 0.5)) + 
+  stat_summary(fun.data=data_summary)
+vplot
+
+
+#paid citation advantage per field
+vplot <- ggplot(merged,aes(x=field,y=paid_cit_diff)) +
+  geom_violin(trim=FALSE) 
+vplot <- vplot + ggtitle("Citation Advantage per Field") +
+  xlab("Field") + ylab("Paid Citation Difference") + 
+  theme(legend.position="none", plot.title=element_text(hjust = 0.5)) + 
+  stat_summary(fun.data=data_summary)
+vplot
+
+#green citation advantage per field
+vplot <- ggplot(merged,aes(x=field,y=green_cit_diff)) +
+  geom_violin(trim=FALSE) 
+vplot <- vplot + ggtitle("Citation Advantage per Field") +
+  xlab("Field") + ylab("Green Citation Difference") + 
   theme(legend.position="none", plot.title=element_text(hjust = 0.5)) + 
   stat_summary(fun.data=data_summary)
 vplot
