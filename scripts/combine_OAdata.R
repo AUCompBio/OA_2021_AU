a40a0cdc12b348ee11de70fb82efd2becb6c04e6
Laurie Stevison <lss0021@CSM-LSS0021-M1MINI.local>
2021-07-13 14:06:05 -0500

    comment out code in progress

--- scripts/combine_OAdata.R
+++ scripts/combine_OAdata.R
@@ -190,8 +190,8 @@ datum <- datum %>% add_column(auth_count = str_count(datum$Authors, ";") + 1)
 #datum <- datum %>% add_column(auth_loc = str_remove(word(datum$corrAuth_loc, -1),"[.]"))
 #datum$auth_loc=str_remove(gsub(".*,","",corr_auth_full),"[.]")
 datum <- datum %>% add_column(corr_auth_count = str_count(datum$corrAuth_loc, ";") + 1)
-datum$auth_loc=ifelse(datum$corr_auth_count>1,,
-                      ifelse(str_detect(datum$corrAuth_loc,"USA"),str_remove(word(datum$corrAuth_loc, -1),"[.]"),str_remove(gsub(".*,","",datum$corrAuth_loc),"[.]")))
+#datum$auth_loc=ifelse(datum$corr_auth_count>1,,
+#                      ifelse(str_detect(datum$corrAuth_loc,"USA"),str_remove(word(datum$corrAuth_loc, -1),"[.]"),str_remove(gsub(".*,","",datum$corrAuth_loc),"[.]")))
 datum$auth_loc=str_trim(datum$auth_loc, side = "left")
 
 #list all countries
