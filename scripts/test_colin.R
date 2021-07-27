4150dd9e6e461e47c9a026b743116543a68514ed
Tanner C. Myers <tannercmyers@gmail.com>
2021-07-27 16:28:48 -0500

    Adding preamble to script

---
+++ scripts/test_colin.R
@@ -0,0 +1,16 @@
+
+
+# the following code performs correlation tests on all parameters used in the models
+# of `analyze_OAdata.R` and `analyze_OAdata_filtered.R` to identify if any violate
+# assumption of no multicollinearity
+
+# clear workspace
+rm(list=ls(all.names=TRUE))
+
+datum <- read_csv("data/OA_data_fin.csv", col_names = TRUE)
+
+
+# check data
+names(datum)
+head(datum)
+summary(datum)
