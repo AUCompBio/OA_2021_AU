1f9bba166d5eb2635704f21693b3ac004c52ea52
Tanner C. Myers <tannercmyers@gmail.com>
2022-02-03 11:04:42 -0600

    Checking for collinearity between JCR and AIS.
    To determine if it was appropriate to include both AIS and JCR in our final model, I updated the variables we check for collinearity in this script, dropping APC in the process. I also added a command to load the package `tidyverse` because this script uses a command from that package.

--- scripts/test_colin.R
+++ scripts/test_colin.R
@@ -1,8 +1,8 @@
-
-
 # the following code performs correlation tests on all parameters used in the models
 # of `analyze_OAdata.R` and `analyze_OAdata_filtered.R` to identify if any violate
 # assumption of no multicollinearity
+
+library(tidyverse)
 
 # clear workspace
 rm(list=ls(all.names=TRUE))
@@ -16,8 +16,13 @@ summary(datum)
 
 
 # isolate numerical variable columns
-OAnum <- datum %>% select(APC, auth_count, AIS, year)
+OAnum <- datum %>% select(auth_count, AIS, JCR_quart, year)
 
 # generate correlation matrix
 OAcor <- cor(OAnum)
-round(OAcor, 2)
+round(OAcor, digits = 2)
+#             auth_count AIS JCR_quart  year
+#auth_count          1    NA        NA    NA
+#AIS                NA  1.00     -0.34 -0.01
+#JCR_quart          NA -0.34      1.00  0.01
+#year               NA -0.01      0.01  1.00
